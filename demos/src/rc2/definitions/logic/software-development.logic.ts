8ec6850cae5de7400eb42a401c73452ddeecd029
Charles Tabor <ct37@txstate.edu>
2026-05-01 09:28:35 -0500

    Add rc2 spec preprocessing file checksum for Logical Thinking section

--- demos/src/rc2/definitions/logic/software-development.logic.ts
+++ demos/src/rc2/definitions/logic/software-development.logic.ts
@@ -1,12 +1,20 @@
 import { type LogicalThinkingApplicantPromptData } from '../models/software-development.models.js'
+import { fileHandler } from 'fastify-txstate'
 
-export function logicalThinkingApplicantPromptPreProcessData(
+export async function logicalThinkingApplicantPromptPreProcessData(
   data: LogicalThinkingApplicantPromptData,
   ctx: any,
   appRequest: any,
   appRequestData: any,
   allPeriodConfig: any,
   db: any
-): LogicalThinkingApplicantPromptData {
+): Promise<LogicalThinkingApplicantPromptData> {
+  if (data.documentation) {
+    for await (const file of ctx.files()) {
+      const { checksum } = await fileHandler.put(file.stream)
+      data.documentation.shasum = checksum
+      break
+    }
+  }
   return data
 }
